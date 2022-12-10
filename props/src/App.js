821bc6326d7919b1dd1f0850ca37acc20ecea97a
yeebryan <bryanwebiz@gmail.com>
2022-12-10 14:47:52 +0800

    update answer

---
+++ props/src/App.js
@@ -0,0 +1,12 @@
+import SumOfTwo from "./SumOfTwo";
+
+function App(){
+  let num = 10;
+  return (
+    <div >
+    <SumOfTwo num1 = {parseInt("30")} num2={num} />
+    </div>
+    );
+}
+
+export default App;
---
+++ props/src/SumOfTwo.js
@@ -0,0 +1,8 @@
+export default function SumOfTwo(props){
+    return (
+        <div>
+            {props.num1 + props.num2}
+        </div>
+    );
+
+}
